3c4e212eb1f4fbf53e4bff149f8298f551aa11a0
Greniel Galinato <greniel.galinato@gmail.com>
2024-12-19 11:44:27 +0800

    js: adding js revisions files

--- js/main.js
+++ js/main.js
@@ -3,21 +3,6 @@ $(() => {
   $('[data-copyright-year]').html(new Date().getFullYear());
 });
 
-(function(l) {
-  if (!l) {
-    window.lintrk = function(a, b) {
-      window.lintrk.q.push([a, b])
-    };
-    window.lintrk.q = []
-  }
-  var s = document.getElementsByTagName("script")[0];
-  var b = document.createElement("script");
-  b.type = "text/javascript";
-  b.async = true;
-  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-  s.parentNode.insertBefore(b, s);
-})(window.lintrk);
-
 document.addEventListener('DOMContentLoaded', () => {
   const lenis = new Lenis({
     lerp: 0.09,
